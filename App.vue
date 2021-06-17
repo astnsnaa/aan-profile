072c9a1e061bafee96ed59fc32b0d24137e2ecb3
aan <85672317+astnsnaa@users.noreply.github.com>
2021-06-17 13:42:23 +0700

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,18 @@
+<template>
+    <Test/>
+</template>
+
+<script>
+import Test from './components/Test.vue'
+
+export default {
+ 
+  components: {
+    Test
+  }
+}
+</script>
+
+<style src="./assets/style.css">
+
+</style>
